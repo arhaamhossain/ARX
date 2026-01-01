f0f8b16dd86a4db67788325586ff1d56755df36f
Arhaam Hossain <arhaamhossain2004@gmail.com>
2026-01-01 18:46:59 -0500

    Refine Stony Brook Robotics Team description for better clarity

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,7 +68,7 @@ const categories: ProjectCategory[] = [
       },
       {
         title: "Stony Brook Robotics Team",
-        description: "Engineering complex robotic systems from concept to competition, integrating electrical, mechanical, and software engineering.",
+        description: "The Stony Brook Robotics Team brings complex robotic systems from concept to competition, integrating electrical, mechanical, and software engineering.",
         highlights: [],
         github: "https://github.com/sbroboticsteam",
         subProjects: [
